b6bff5322aeccb3a95cc473ef51d7ad7ced615f5
makramab <m.akram.bari@nyu.edu>
2025-02-02 11:38:35 -0500

    test: comment supposed rendering diagram code

--- lua/diagram/init.lua
+++ lua/diagram/init.lua
@@ -130,20 +130,20 @@ local toggle_rendering = function(action, opts)
 		else
 			clear_buffer(bufnr)
 		end
-	else
-		if not state.globally_disabled then
-			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-				for _, integration in ipairs(state.integrations) do
-					if vim.tbl_contains(integration.filetypes, vim.bo[buf].filetype) then
-						render_buffer(buf, vim.api.nvim_get_current_win(), integration)
-					end
-				end
-			end
-		else
-			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-				clear_buffer(buf)
-			end
-		end
+		-- else
+		-- 	if not state.globally_disabled then
+		-- 		for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		-- 			for _, integration in ipairs(state.integrations) do
+		-- 				if vim.tbl_contains(integration.filetypes, vim.bo[buf].filetype) then
+		-- 					render_buffer(buf, vim.api.nvim_get_current_win(), integration)
+		-- 				end
+		-- 			end
+		-- 		end
+		-- 	else
+		-- 		for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		-- 			clear_buffer(buf)
+		-- 		end
+		-- 	end
 	end
 end
 
